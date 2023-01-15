c744398443d4808105a4b8bd42cafa22f2c5d242
Gulshan706 <113226798+Gulshan706@users.noreply.github.com>
2023-01-15 22:20:00 +0530

    Create 0002UnionOfArray.java

---
+++ Code/0002UnionOfArray.java
@@ -0,0 +1,33 @@
+    /*
+    
+    Problem Statement
+    -----------------
+    Given two Array and find the set of that array.
+    
+    Time Complexity  : 
+    Space Complexity :
+    
+    */
+
+
+class UnionOfSet{
+
+public static int[] union(int [] set1, int [] set2) {
+		HashSet<Integer> set= new HashSet<Integer>();
+	   
+		for(int i=0;i<set1.length;i++)
+			set.add(set1[i]);
+		
+		for(int i=0;i<set2.length;i++)
+			set.add(set2[i]);
+		
+		int[] union = new int[set.size()];
+		int count=0;
+		
+		for(int i:set)
+			union[count++]=i;
+		
+	return union;
+	
+	}
+}
