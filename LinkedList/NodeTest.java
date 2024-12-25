3e7102d9e8b176751038692338cab04dee92dfd5
Gulshan706 <113226798+Gulshan706@users.noreply.github.com>
2024-12-25 05:55:54 +0530

    Update NodeTest.java

--- LinkedList/NodeTest.java
+++ LinkedList/NodeTest.java
@@ -12,13 +12,20 @@ class NodeTest{
     public static void main (String[] args) {
 
     Node head=new Node(10);
-    Node temp1=new Node(20);
+   /* 
+   Node temp1=new Node(20);
     Node temp2= new Node(30);
     Node temp3=new Node(40);
     
     head.next=temp1;
     temp1.next=temp2;
     temp2.next=temp3;
+    */
+    Node head=new Node(10);
+    head.next=new Node(20);
+    head.next.next= new Node(30);
+    head.next.next.next=new Node(40);
+        
     printList(head);
     
 }
